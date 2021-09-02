691634e950d17b261d9974e6e55dcdbaa14ca12b
Toasty <toasty65.dev@gmail.com>
2021-09-02 23:10:52 +0200

    Changed schedule for member count update back

--- util/updater.js
+++ util/updater.js
@@ -3,7 +3,7 @@ const schedule = require('node-schedule');
 module.exports = {
     execute: (client) => {
         // Run this job every 2 hours
-        const updateMemberCountJob = schedule.scheduleJob('* * * * *', () => {
+        const updateMemberCountJob = schedule.scheduleJob('0 */2 * * *', () => {
             require('../options/jobs/updateMemberCount').execute(client);
         });
     }
