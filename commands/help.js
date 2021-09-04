ff06980985e4bed8dc16adb2f6f0aae9fa97090a
Toasty <toasty65.dev@gmail.com>
2021-09-04 22:59:42 +0200

    Quick hotfix for help command

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
   name: 'help',
   data: new SlashCommandBuilder()
-		.setName('help1')
+		.setName('help')
 		.setDescription('Shows help about the usage.'),
 	async execute(client, interaction) {
 		await interaction.reply('Nicht so eilig, kommt schon noch!');
