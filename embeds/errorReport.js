103095beb02b9ac8407c742dc95689e7a04c598d
Toasty <toasty65.dev@gmail.com>
2021-09-13 23:24:00 +0200

    Errors will now be sent to the specified developer via DM

---
+++ embeds/errorReport.js
@@ -0,0 +1,32 @@
+module.exports = function (info) {
+    const { error, guild, time, user, channel, icon, usedCommand } = info
+    return {
+          "title": 'Used Command: `' + usedCommand + '`',
+          "color": 0xff3333,
+          "timestamp": time,
+          "thumbnail": {
+            "url": icon
+          },
+          "author": {
+            "name": `❗ Error in guild ${guild} ❗`,
+            "icon_url": icon
+          },
+          "fields": [
+            {
+              "name": "**Channel:**",
+              "value": channel,
+              "inline": true
+            },
+            {
+                "name": "**Used by:**",
+                "value": user,
+                "inline": true
+            },
+            {
+                "name": "**Error:**",
+                "value": '`' + error + '`',
+                "inline": false
+            },
+          ]
+      }
+  };
--- events/client/interactionCreate.js
+++ events/client/interactionCreate.js
@@ -41,10 +41,31 @@ module.exports = async (client, interaction) => {
               embeds: [client.responseEmbeds.get("successful").embed],
             });
           } catch (error) {
-            console.log(error);
+            // Create info object for error report
+            const time = new Date().toUTCString();
+            const info = {
+              error,
+              guild: interaction.guild.name,
+              time,
+              user: interaction.member.nickname,
+              channel: interaction.channel.name,
+              icon: interaction.guild.iconURL(),
+              usedCommand: element,
+            };
+
+            // Send error report
+            await interaction.guild.members.fetch('224501142237741057')
+            .then(member => {
+                member.send({
+                embeds: [require('../../embeds/errorReport')(info)]
+              })
+            })
+            .then(() => console.log('Error report sent'))
+            .catch(() => console.log(`Couldn't send error report`));
+
             return await interaction.editReply({
-              content: "<@224501142237741057>",
-              embeds: [require('../../embeds/errorOutput')('An error has occured!\n\n`' + error + '`')],
+              content: " ",
+              embeds: [require('../../embeds/errorOutput')('An error has occured!')],
             });
           }
         });
